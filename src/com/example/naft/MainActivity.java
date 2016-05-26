5bd301a8ec26eb97657e7f03f3afa161a7f2ef12
Hossein Yousefi <hossein.yousefi23@gmail.com>
2016-05-27 01:30:26 +0430

    changed auto generated files

--- src/com/example/naft/MainActivity.java
+++ src/com/example/naft/MainActivity.java
@@ -4,17 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
-import android.app.Fragment;
-import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.CheckBox;
 import android.widget.ListView;
 
